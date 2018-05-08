5d3104c823b2c0e0e89a4e378aedaad014e55010
Alexandr Tikhonov <t9101029991@gmail.com>
2018-05-08 14:41:14 +0300

    add viewport

--- views/layouts/demo.php
+++ views/layouts/demo.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name='viewport' content='width=1024'>
+  <meta name='viewport' content='width=device-width'>
   <title>The Village Cronos</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
