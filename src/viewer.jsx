c5326f3fc5d73089cd76a36fcaa197377b54d932
Alexandr Tikhonov <t9101029991@gmail.com>
2018-05-10 20:13:20 +0300

    clear viewer block

--- src/viewer.jsx
+++ src/viewer.jsx
@@ -6,15 +6,6 @@ const Viewer = props => {
     <div id='canvasContainer' style={{display: props.canvasMode ? 'block' : 'none'}}>
     </div>
     {props.mainImage && !props.canvasMode ? <img id='mainProductImage' className='card-img-top' src={props.mainImage} /> : false}
-    <div className="mainProductView-description">
-      <div className="mainProductView-title">{props.currentProduct.brand + ' ' + props.currentProduct.sku}</div>
-      <div className='mainProductView-color'>{props.currentProduct.color}</div>
-      <div className='mainProductView-price'>
-        <span>{props.currentProduct.pricediscount == null ? props.currentProduct.price : props.currentProduct.pricediscount}</span>
-        <span> <s>{props.currentProduct.pricediscount == null ? null : props.currentProduct.price}</s></span>
-      </div>
-      <a href={props.currentProduct.link} target='_blank' className="btn btn-dark">Купить в Cronos</a>
-    </div>
   </div>);
 };
 
