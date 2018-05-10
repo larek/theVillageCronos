dc512333f280d631fb2e5c13ca47e360fb2c96f9
Alexandr Tikhonov <t9101029991@gmail.com>
2018-05-10 17:24:21 +0300

    gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -27,3 +27,50 @@ gulp.task('thumbs', () => {
     });
   });
 });
+
+
+gulp.task('png2jpg', () => {
+  fs.readdir(dir, (err, list) =>{
+    list.forEach(item => {
+      let stats = fs.statSync(path.join(dir + '/' + item));
+      if(stats.isDirectory()){
+        fs.readdir(dir + '/' + item, (e, l) => {
+          l.forEach(i => {
+            // crop and resize faces
+            if(i == '3.png' || i == '4.png'){
+              let basename = path.basename(dir + '/' + item + '/' + i, '.png');
+              gm(dir + '/' + item + '/' + i)
+                .resize(300)
+                .write(dir + '/' + item + '/' + basename +'.jpg', err => {
+                  err ? console.log(err) : false; // eslint-disable-line no-console
+                });
+            }
+          });
+        });
+      }
+    });
+  });
+});
+
+
+gulp.task('png', () => {
+  fs.readdir(dir, (err, list) =>{
+    list.forEach(item => {
+      let stats = fs.statSync(path.join(dir + '/' + item));
+      if(stats.isDirectory()){
+        fs.readdir(dir + '/' + item, (e, l) => {
+          l.forEach(i => {
+            // crop and resize faces
+            if(i == '5.png'){
+              gm(dir + '/' + item + '/' + i)
+                .resize(300)
+                .write(dir + '/' + item + '/5.png', err => {
+                  err ? console.log(err) : false; // eslint-disable-line no-console
+                });
+            }
+          });
+        });
+      }
+    });
+  });
+});
