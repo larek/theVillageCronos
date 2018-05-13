24d04573ae425b2ddb5e4eb8cb5bd3e3900b57e3
Alexandr Tikhonov <t9101029991@gmail.com>
2018-05-14 02:09:05 +0300

    assets version

--- views/layouts/demo.php
+++ views/layouts/demo.php
@@ -5,7 +5,7 @@
   <meta name='viewport' content='width=device-width'>
   <title>The Village Cronos</title>
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
-  <link rel="stylesheet" href="/css/bundle.css?v=1.0.8">
+  <link rel="stylesheet" href="/css/bundle.css?v=1.0.9">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.2/croppie.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
@@ -14,6 +14,6 @@
   <script src='https://cdnjs.cloudflare.com/ajax/libs/fabric.js/2.2.3/fabric.min.js'></script>
   <script src='https://cdnjs.cloudflare.com/ajax/libs/exif-js/2.3.0/exif.js'></script>
   <script src='https://pixlcore.com/demos/webcamjs/webcam.min.js'></script>
-  <script src='/js/bundle.js?v=1.0.9'></script> 
+  <script src='/js/bundle.js?v=1.1.0'></script> 
 </body>
 </html>
